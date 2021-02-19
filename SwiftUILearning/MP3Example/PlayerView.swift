7aa77bfd68922f6b76d5869fe8e93c71aba086a4
Rohit <rohitchaurasiya.dcs19@vnsgu.ac.in>
2021-02-19 18:25:01 +0530

    remove comments from Playerview screen

--- SwiftUILearning/MP3Example/PlayerView.swift
+++ SwiftUILearning/MP3Example/PlayerView.swift
@@ -7,50 +7,6 @@
 
 import SwiftUI
 import AVKit
-//
-//struct PlayerView: View,ObservableObject {
-//    @Published var playValue: TimeInterval = 0.0
-//    @State var audioPlayer: AVAudioPlayer!
-//    var playing = false
-//    var playerDuration: TimeInterval = 146
-//    var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-//    var body: some View {
-//        ZStack {
-//            VStack {
-//                Text("Play").font(.system(size: 45)).font(.largeTitle)
-//                HStack {
-//                    Spacer()
-//                    Button(action: {
-//                        self.audioPlayer.play()
-//                    }) {
-//                        Image(systemName: "play.circle.fill").resizable()
-//                            .frame(width: 50, height: 50)
-//                            .aspectRatio(contentMode: .fit)
-//                    }
-//                    Spacer()
-//                    Button(action: {
-//                        self.audioPlayer.pause()
-//                    }) {
-//                        Image(systemName: "pause.circle.fill").resizable()
-//                            .frame(width: 50, height: 50)
-//                            .aspectRatio(contentMode: .fit)
-//                    }
-//                    Spacer()
-//                }
-//            }
-//        }
-//        .onAppear {
-//            let sound = Bundle.main.path(forResource: "song", ofType: "mp3")
-//            self.audioPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
-//        }
-//    }
-//}
-//
-//struct PlayerView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PlayerView()
-//    }
-//}
 
 class audioSettings: ObservableObject {
     var audioPlayer: AVAudioPlayer?
